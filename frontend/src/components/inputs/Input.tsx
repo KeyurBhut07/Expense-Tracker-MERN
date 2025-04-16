06693eea689236ccfe252e647428987635aaceb3
keyur-cogtix <keyur.b@cogtix.com>
2025-04-16 10:35:41 +0530

    login page design done

--- frontend/src/components/inputs/Input.tsx
+++ frontend/src/components/inputs/Input.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {FaRegEye, FaRegEyeSlash} from "react-icons/fa6"
 
 interface InputProps {
   value: string;
@@ -8,10 +9,51 @@ interface InputProps {
   type?: string;
 }
 
+const Input = ({
+  value,
+  onChange,
+  label,
+  placeholder,
+  type = 'text',
+}: InputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <div>
+      <label className="text-[13px] text-slate-800">{label}</label>
 
-
-const Input: React.FC<InputProps> = ({ value, onChange, label, placeholder, type = 'text' }) => {
-  return <div>Input</div>;
+      <div className='input-box'>
+        <input
+          type={
+            type === 'password' ? (showPassword ? 'text' : 'password') : type
+          }
+          placeholder={placeholder}
+          className="w-full bg-transparent outline-none"
+          value={value}
+          onChange={(e: any) => onChange(e)}
+        />
+        {type === 'password' && (
+          <>
+          {showPassword ? (
+            <FaRegEye
+            size={22}
+            className="text-primary cursor-pointer"
+            onClick={() => toggleShowPassword()}
+            />
+          ) : (
+            <FaRegEyeSlash
+            size={22}
+            className="text-slate-400 cursor-pointer"
+            onClick={() => toggleShowPassword()}
+            />
+          )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default Input
+export default Input;
--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/inputs/Input';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
+  const [error, setError] = useState('');
 
   const navigate = useNavigate();
 
   // Handle Login Form submit
-  const handleLogin = (e: any) => {};
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address');
+      return;
+    }
+    if (!password) {
+      setError('Please enter a password');
+      return;
+    }
+    setError('');
+
+    // Login API Call
+  };
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -28,6 +42,29 @@ const Login = () => {
             placeholder="john@example.com"
             type="text"
           />
+          <Input
+            value={password}
+            onChange={(e: any) => setPassword(e.target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{' '}
+            <Link
+              className="font-medium text-primary underline"
+              to={'/register'}
+            >
+              Register
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
