4e349a2af444be8fc660c2eb942a01955dd33bc8
keyur-cogtix <169987382+keyur-cogtix@users.noreply.github.com>
2025-04-18 22:04:41 +0530

    register page design done

---
+++ frontend/src/components/inputs/ProfilePhotoSelector.tsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from 'react';
+import { LuUser, LuUpload, LuTrash } from 'react-icons/lu';
+
+interface ProfilePhotoSelectorProps {
+  image: File | null | string;
+  setImage: (file: File | null) => void;
+}
+
+const ProfilePhotoSelector: React.FC<ProfilePhotoSelectorProps> = ({
+  image,
+  setImage,
+}) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setImage(file);
+
+      // Genrate priview URL from the file
+      const preview = URL.createObjectURL(file);
+      setPreviewUrl(preview);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+    setPreviewUrl(null);
+  };
+
+  const onChooseFile = () => {
+    inputRef.current?.click();
+  };
+
+  return (
+    <div className="flex justify-center mb-6">
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
+
+      {!image ? (
+        <div className="w-20 h-20 flex items-center justify-center bg-purple-100 rounded-full relative">
+          <LuUser className="text-4xl text-primary" />
+
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1"
+            onClick={onChooseFile}
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="relative">
+            <img
+              src={previewUrl || ''}
+              alt="Profile Photo"
+              className="w-20 h-20 rounded-full object-cover"
+            />
+            <button
+              type="button"
+              className="w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1"
+              onClick={handleRemoveImage}
+            >
+              <LuTrash />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePhotoSelector;
--- frontend/src/pages/Auth/SignUp.tsx
+++ frontend/src/pages/Auth/SignUp.tsx
@@ -1,5 +1,96 @@
+import { useState } from 'react';
+import AuthLayout from '../../components/layouts/AuthLayout';
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/inputs/Input';
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/inputs/ProfilePhotoSelector';
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [profilePic, setProfilePic] = useState<File | string | null>(null);
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async (e: any) => {
+    e.preventDefault();
+    let profileImageUrl = '';
+    if (!fullName) {
+      setError('Please enter you name');
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email');
+      return;
+    }
+
+    if (!password) {
+      setError('Please enter a password');
+      return;
+    }
+    setError('');
+
+    // Sign UP API Call
+  };
+
+  return (
+    <AuthLayout>
+      {/* <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex-col justify-center"> */}
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create a Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.{' '}
+        </p>
+
+        <form onSubmit={handleRegister}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={(e: any) => setFullName(e.target.value)}
+              label="Full Name"
+              placeholder="john"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={(e: any) => setEmail(e.target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+                label="Password"
+                placeholder="Min 8 Characters"
+                type="password"
+              />
+            </div>
+          </div>
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            Register
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?{' '}
+            <Link className="font-medium text-primary underline" to={'/login'}>
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default SignUp;
